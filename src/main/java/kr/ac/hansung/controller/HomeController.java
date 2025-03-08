fe54d3c705b613d01b41f75692e60862e4a30279
wjlee <000jun2626@gmail.com>
2025-03-08 23:02:47 +0900

    웹프레임워크2 1주차 정리

--- src/main/java/kr/ac/hansung/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/controller/HomeController.java
@@ -11,20 +11,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    //private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    // 로거 객체를 생성. 로거는 "kr.ac.hansung.controller.HomeController" 이름으로 설정.
     private static final Logger logger = LoggerFactory.getLogger("kr.ac.hansung.controller.HomeController");
 
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(HttpServletRequest request) {
+    // 루트 URL ("/")로 들어오는 GET 요청을 처리하는 메서드.
+    // get메서드 로 요청이 들어오면 home() 메서드가 호출됨.
 
+    public String home(HttpServletRequest request) {
+        // 요청 URL을 추출
         String url = request.getRequestURL().toString();
+        // 클라이언트의 IP 주소를 추출
         String clientIPaddr = request.getRemoteAddr();
 
+        // 추출한 URL과 IP 주소를 로깅
         logger.info("Request URL: {}, Client IP: {}", url, clientIPaddr);
 
+        // home.jsp라는 뷰를 클라이언트에게 응답으로 반환
+        // 즉, "/" 루트 URL로 들어온 요청에 대해 -> home.jsp를 서블릿처리를 거쳐 뷰(html)로 반환해주는 역할을 하는 메서드
         return "home";
     }
-}
 
+}
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,9 +1,11 @@
+<%--스프링 프로젝트는 다음과 같은 방식으로 동작-->
 <%--
-  Created by IntelliJ IDEA.
-  User: nykim
-  Date: 2022/12/13
-  Time: 12:55 오전
-  To change this template use File | Settings | File Templates.
+1. 프로젝트 빌드: Maven을 사용하여 프로젝트를 빌드하고, war 아티팩트를 생성.
+2. Tomcat 서버에 배포: 생성된 war 파일을 Tomcat 서버에 배포.
+3. 서버 실행: Tomcat 서버가 실행되면, webapps 폴더에 있는 war 파일을 자동으로 찾아서 압축을 풀고, 애플리케이션을 배포. 이 과정에서 web.xml 파일을 읽고, 서블릿과 리스너 등을 설정하여 애플리케이션을 준비.
+4. 요청 처리: 클라이언트가 Tomcat 서버의 특정 포트(8080)로 요청을 보냄.
+5. 컨트롤러 처리 & 뷰 렌더링: HomeController가 루트 URL (/)로 들어오는 GET 요청을 처리 -> home.jsp 파일이 서블릿에 의해 처리되어 HTML로 컴파일됨 -> 클라이언트에게 응답으로 HTML 페이지(뷰)가 전송됨.
+따라서, Tomcat 서버가 실행 중인 포트 (예: http://localhost:8080/)에 접속하면 home.jsp가 렌더링된 HTML 페이지를 볼 수 있게되는 것임.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
